6bcebdb2ebcbcaf6355d38f12e9b0e56632e6afd
Brian Donnoe <dunow33@hotmail.com>
2024-10-12 18:39:17 -0400

    getting things set up

--- Budget.Mvc/Repositories/BudgetRepository.cs
+++ Budget.Mvc/Repositories/BudgetRepository.cs
@@ -73,21 +73,9 @@ namespace Budget.Mvc.Repositories
             return await budgetDbContext.Categories.ToListAsync();
         }
 
-        public List<Transaction> GetTransactions()
+        public async Task<List<Transaction>> GetTransactions()
         {
-            //using (IDbConnection connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
-            //{
-            //    var query =
-            //        @"SELECT t.Amount, t.CategoryId, t.[Date], t.Id, t.TransactionType, t.Name, c.Name AS Category
-            //          FROM Transactions AS t
-            //          LEFT JOIN Category AS c
-            //          ON t.CategoryId = c.Id;";
-
-            //    var allTransactions = connection.Query<Transaction>(query);
-
-            //    return allTransactions.ToList();
-            //}
-            return null;
+            return await budgetDbContext.Transactions.ToListAsync();
         }
 
         public void UpdateTransaction(Transaction transaction)
